87cf0a8c8e5d13996969e3a4cf6108149980e310
Ashish mahto <mahtoashish>
2023-12-04 17:46:11 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+{/* <script type="text/javascript"> */}
+function imgSlider(anything) {
+    document.querySelector('.starbucks').src = anything;
+}
+
+function changeCircleColor(color) {
+    const circle = document.querySelector('.circle');
+    circle.style.background = color;
+}
